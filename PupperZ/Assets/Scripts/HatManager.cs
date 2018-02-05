b5ccc7717aab2e45dd76e1680ef38ab5d814e15e
Londonsklart <karlfyrkant@gmail.com>
2018-02-05 14:18:22 +0100

    WinScreen, HatImplement, Smaller fixes

---
+++ PupperZ/Assets/Scripts/HatManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatManager : MonoBehaviour
+{
+    GameObject hatLocation;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "PupperZ")
+        {
+            gameObject.transform.position = new Vector3(0, 0, 0);
+            hatLocation = GameObject.FindGameObjectWithTag("Hatlocation");
+            Instantiate(gameObject, hatLocation.transform);
+            Destroy(gameObject);
+
+        }
+    }
+
+}
